6d3c9d0b68e756bf438eb99bac1ed1d16afdada0
Tim Chang <tim.chang87@gmail.com>
2026-06-19 14:29:58 -0400

    Remove redundant npm test scripts.

--- services/core/src/app.ts
+++ services/core/src/app.ts
@@ -3,7 +3,7 @@ import { SERVER_PORT } from './config.js';
 import cors from 'cors';
 
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3000' }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:8080' }));
 
 app.get('/test', (_req, res) => {
   res.json({ message: 'Hello from the server!' }).status(200);
